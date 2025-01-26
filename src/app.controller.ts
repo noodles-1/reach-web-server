96533fd14dbb2ed88955e7eb27b61ba55ac4ef62
Adriane Gil Roa <adriane.roa@hoolisoftware.com>
2025-01-26 10:23:25 +0800

    feat: add get and post routes

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,29 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import { AppService } from '@/app.service';
+import { ItemDto } from '@/item.dto';
 
 @Controller()
 export class AppController {
     constructor(private readonly appService: AppService) {}
 
+    /**
+     * GET REQUESTS
+     */
     @Get()
     async getItems() {
         return this.appService.getItems();
+    }
+
+    @Get(':name')
+    async getItem(@Param() params: ItemDto) {
+        return this.appService.getItem(params.name);
+    }
+
+    /**
+     * POST REQUESTS
+     */
+    @Post('add-item')
+    async addItem(@Body() itemDto: ItemDto) {
+        return this.appService.addItem(itemDto.name);
     }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -7,4 +7,19 @@ export class AppService {
     async getItems(): Promise<any[]> {
         return await this.sql(`SELECT * FROM "Item"`);
     }
+
+    async getItem(name: 'bottle' | 'utensil'): Promise<any> {
+        return await this.sql(`
+            SELECT * FROM "Item"
+            WHERE name = '${name}'
+            ORDER BY detected_on ASC   
+        `)
+    }
+
+    async addItem(name: 'bottle' | 'utensil'): Promise<any> {
+        return await this.sql(`
+            INSERT INTO "Item" (name) 
+            VALUES ('${name}')
+        `);
+    }
 }
